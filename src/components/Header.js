73069653fcd3b175e93794c4cfc8af3eb700230a
Will <28814921+wkey1980@users.noreply.github.com>
2021-09-20 22:02:11 +0100

    Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,71 @@
 import React from 'react'
+import styled from 'styled-components/macro'
+import MenuIcon from '@material-ui/icons/Menu'
 
 const Header = () => {
 	return (
-		<div>
-			<h1>Header.JS</h1>
-		</div>
+		<Container>
+
+			<a>
+				<img src="/images/logo.svg" alt="Logo" />
+			</a>
+			<Menu>
+				<a href="#">Model S</a>
+				<a href="#">Model 3</a>
+				<a href="#">Model X</a>
+				<a href="#">Model Y</a>
+			</Menu>
+			<RightMenu>
+				<a href="#">Shop</a>
+				<a href="#">Tesla Account</a>
+				<CustomMenu />
+			</RightMenu>
+
+		</Container>
 	)
 }
+
+
+const Container = styled.div`
+	min-height: 60px;
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+
+	display: flex;
+	/* justify-content: center; */
+	align-items: center;
+
+	padding: 0 20px;
+
+	background-color: teal;
+`;
+const Menu = styled.div`
+	display: flex;
+	justify-content: center;
+	flex: 1;
+	align-content: center;
+
+	a {
+		font-weight: 600;
+		text-transform: uppercase;
+		padding: 0 10px;
+		flex-wrap: nowrap;
+	}
+`;
+
+const RightMenu = styled.div`
+
+	a {
+		font-weight: 600;
+		text-transform: uppercase;
+		margin-right: 10px;
+	}
+`;
+
+const CustomMenu = styled(MenuIcon)`
+
+`;
 
 export default Header
